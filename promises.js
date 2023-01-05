e25d3db290ef6e521fb0c54df3e1d1a69f90c34f
Shubham Mali <shubhammali31299@gmail.com>
2023-01-05 07:52:42 +0530

    started with promise---imp

---
+++ promises.js
@@ -0,0 +1,6 @@
+// Promise --------> Very important topic
+
+// Promise ek aisi value hai jo hame abhi nahi pata but hume future mai patah hogi
+// promise represent karta hai future value jo hume abhi nahi pata but badme pata chalegi
+
+// watch promise egain
